36eb37365d541f4d14b6227c079765bbc97ae777
safewordteacup <tomslaganovskis@gmail.com>
2022-10-18 13:51:15 +0300

    Class_

---
+++ Class_11/if_tests.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+	if(1);// ieksh () var buut "apaljaa" nulle (visi biti ir ar 0)
+		//vai bitu seciiba kas satur kaut vienu bitu ar 1
+	int a =10;
+	if(a>7)//a>7 -> 0000 0000 0000 0000 0000 0000 0000 0001
+	printf("Viena darbiiba, kas tiks izpildiita, ja \"nosaciijums ir speekaa\".\n");
+	printf("shii darbiiba tiks ipildiita jebkuraa gadijumaa. \n");
+
+	if(a>7)
+		{
+		printf("Darbiibu bloks, kas tiks ipildiits, ja \"nosaciijumsir speekaa\"");
+		printf("Pirmaa darbiiba \n");
+		printf("Otraa darbiiba \n");
+		}
+	if(a>7)
+		{
+		printf("Darbiibu bloks, kas tiks ipildiits, ja \"nosaciijums ir speekaa\"");
+		printf("Pirmaa darbiiba \n");
+		printf("Otraa darbiiba \n");
+		}
+	else
+		{
+		printf("Darbiibu bloks, kas tiks ipildiits, ja \"nosaciijums nav speekaa\"");
+		printf("Pirmaa darbiiba \n");
+		printf("Otraa darbiiba \n");
+		}
+return 0:
+}
