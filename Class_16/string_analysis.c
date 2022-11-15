17156f30d710d7ff06cc6f55127b2e6111d0dc6a
safewordteacup <tomslaganovskis@gmail.com>
2022-11-15 09:54:19 +0200

    Class_

---
+++ Class_16/string_analysis.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int y = 0;
+char sentence[100];
+int main()
+{
+	printf("Enter a sentence with no more than 99 unmodified ascii symbols: \n");
+	scanf("%s", sentence);
+	
+	return 0;
+}
