468d4cf44598433cc7a946aa3c371fc3677fd03d
safewordteacup <tomslaganovskis@gmail.com>
2023-01-27 02:33:48 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

---
+++ Laboratory_assignments/LD3/Derivative+FileIO.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double ex, ex1, ex2, first_deriv, second_deriv, a, b, x, deltaX;
+
+    double myexp(double a){
+        return (1+a)*exp(a);
+    }
+    double myexp1(double x){
+        return 2*exp(x)+x*exp(x);
+    }
+    double myexp2(double x){
+        return 3*exp(x)+x*exp(x);
+    }
+
+int main(){
+
+    FILE *myfile = fopen("derivative.dat", "w");
+
+    if(myfile == NULL){
+        printf("Error.");
+        exit(1);
+    }
+
+    printf("\nEnter value a(left most bounds for function):\n");
+    scanf("%lf",&a);
+    printf("\nEnter value b(right most bounds for function):\n");
+    scanf("%lf",&b);
+    printf("\nEnter precision which to use, to calculate the derivative\n");
+    printf("\nin the form \"0.000...1\":\n");
+    scanf("%lf",&deltaX);
+
+    //table formating stuff
+
+    fprintf(myfile, "\t                x\t              (1+x)*exp(x)\t       ((1+x)*exp(x))\'ana.form.\t        ((1+x)*exp(x))\'dif.form.\t         ((1+x)*exp(x))\'\'ana.form.\t        ((1+x)*exp(x))\'\'dif.form.\n\n");
+    
+    x = a;
+    while(x<b){
+        first_deriv = (((myexp(x+deltaX))-(myexp(x-deltaX)))/(2*deltaX));
+        second_deriv = (((((((myexp(x)+deltaX)-(myexp(x)-deltaX))/(2*deltaX)))+(deltaX))-(((((myexp(x)+deltaX)-(myexp(x)-deltaX))/(2*deltaX)))-(-deltaX)))/(2*deltaX));
+        ex = myexp(x); ex1 = myexp1(x); ex2 = myexp2(x);
+
+        fprintf(myfile, "\t %20.3lf\t %20.3lf\t %20.3lf\t %20.3lf\t %20.3lf\t %20.3lf\n", x, myexp(x), ex1, first_deriv, ex2, second_deriv);
+        x += deltaX;
+
+    } 
+
+    fclose(myfile);
+
+    return 0;
+}
